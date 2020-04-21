592a9252db8c029fbda5b608b30b9fa026f62bbc
zengchuanzhi <1240022018@qq.com>
2020-04-21 17:18:08 +0800

    Default Changelist

--- src/main/java/com/dao/GoodsDao.java
+++ src/main/java/com/dao/GoodsDao.java
@@ -1,5 +1,6 @@
 package com.dao;
 
+import com.pojo.Cart;
 import com.pojo.Comments;
 import com.pojo.Goods;
 import com.pojo.GoodsCollection;
@@ -9,9 +10,20 @@ import java.util.List;
 
 public interface GoodsDao {
     Goods selectGoodsById(Integer id);
+
+    Cart checkInCart(@Param("account") String account, @Param("id") Integer id);
+
+    int updateCart(@Param("account") String account, @Param("id") Integer id, @Param("num") int num);
+
     int addInCart(@Param("account") String account, @Param("id") Integer id, @Param("num") int num);
+
     int addInCollection(@Param("account") String account, @Param("id") Integer id);
+
+    int removeFromCollection(@Param("account") String account, @Param("id") Integer id);
+
     GoodsCollection checkInCollection(@Param("account") String account, @Param("id") Integer id);
+
     List<Comments> selectCommentsByGoodsId(@Param("id") Integer id);
+
     List<Goods> selectRecommentsByCategoryId(@Param("id") Integer id);
 }
