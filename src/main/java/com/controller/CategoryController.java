f0228cf971fab2da54dccc7a5d30bc2a4c5f90f7
unsnow <1739892273@qq.com>
2020-05-05 22:57:28 +0800

    user and category

--- src/main/java/com/controller/CategoryController.java
+++ src/main/java/com/controller/CategoryController.java
@@ -1,4 +1,10 @@
 package com.controller;
 
+import com.pojo.Category;
+
+import java.util.List;
+
 public interface CategoryController {
+    List<Category> getAllTopCategoryController();
+    List<Category> getSubCategoryByIdController(Integer id);
 }
